be00041f85f489c188188bddd8cb32b61593b8a3
Vincent Le Goualher <v.legoualher@gmail.com>
2022-08-25 21:55:48 +0200

    integrate MySQL code with respect to k8s database

--- workdir/app.py
+++ workdir/app.py
@@ -15,6 +15,9 @@ import os
 # db_user = get_secret('FLASK_DB_USER_FILE')
 # db_password = get_secret('FLASK_DB_PASSWORD_FILE')
 
+db_user = "frontend"
+db_password = "password"
+
 # Init Flask app
 app = Flask(__name__)
 
@@ -26,19 +29,19 @@ def home():
         json_data = {'text_de': text_de}
         text_en = requests.post('http://backend-fastapi-service/translate', headers=headers, json=json_data).json()['text_en']
 
-        # if text_de:
-        #     try:
-        #         cnx = mysql.connector.connect(user=db_user, password=db_password, host='database', database='translation')
-        #         cursor = cnx.cursor()
-        #         text_de_sql = text_de.replace("\'", "\\\'")
-        #         text_en_sql = text_en.replace("\'", "\\\'")
-        #         cursor.execute(f"""INSERT INTO translations (text_de, text_en) VALUES ('{text_de_sql}', '{text_en_sql}');""")
-        #         cnx.commit()
-        #     except mysql.connector.Error as error:
-        #         text_de = f'The input and its translation could not be written into the database! The following MySQL error was raised: {str(error)}\n\n{text_de}'
-        #     finally:
-        #         cursor.close()
-        #         cnx.close()
+        if text_de:
+            try:
+                cnx = mysql.connector.connect(user=db_user, password=db_password, host='mysql-headless', database='translation')
+                cursor = cnx.cursor()
+                text_de_sql = text_de.replace("\'", "\\\'")
+                text_en_sql = text_en.replace("\'", "\\\'")
+                cursor.execute(f"""INSERT INTO translations (text_de, text_en) VALUES ('{text_de_sql}', '{text_en_sql}');""")
+                cnx.commit()
+            except mysql.connector.Error as error:
+                text_de = f'The input and its translation could not be written into the database! The following MySQL error was raised: {str(error)}\n\n{text_de}'
+            finally:
+                cursor.close()
+                cnx.close()
 
         return render_template('index.html', input = text_de, translation = text_en)
     else:
@@ -50,23 +53,23 @@ def query_db():
     return render_template('query_db.html', result = None)
 
 
-    # if request.method == 'POST':
-    #     query = request.form['query']
+    if request.method == 'POST':
+        query = request.form['query']
 
-    #     try:
-    #         cnx = mysql.connector.connect(user=db_user, password=db_password, host='database', database='translation')
-    #         cursor = cnx.cursor()
-    #         cursor.execute(query)
-    #         result = pd.DataFrame(data = cursor.fetchall(), columns = cursor.column_names).to_html(index=False)
-    #     except mysql.connector.Error as error:
-    #         result = f'Something went wrong! The following MySQL error was raised: {str(error)}'
-    #     finally:
-    #         cursor.close()
-    #         cnx.close()
+        try:
+            cnx = mysql.connector.connect(user=db_user, password=db_password, host='mysql-headless', database='translation')
+            cursor = cnx.cursor()
+            cursor.execute(query)
+            result = pd.DataFrame(data = cursor.fetchall(), columns = cursor.column_names).to_html(index=False)
+        except mysql.connector.Error as error:
+            result = f'Something went wrong! The following MySQL error was raised: {str(error)}'
+        finally:
+            cursor.close()
+            cnx.close()
 
-    #     return render_template('query_db.html', result = result )
-    # else:
-    #     return render_template('query_db.html', result = None)
+        return render_template('query_db.html', result = result )
+    else:
+        return render_template('query_db.html', result = None)
 
 # Run Flask app. Host and port are specified in the Dockerfile
 if __name__ == "__main__":
